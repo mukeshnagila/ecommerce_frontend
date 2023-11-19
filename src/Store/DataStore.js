99549766b5e625727d185480c45eb1d783cef62b
mukeshnagila <nagilamukesh43@gmail.com>
2023-11-19 23:10:47 +0530

    EcommerceProject_clientSide19112023

--- src/Store/DataStore.js
+++ src/Store/DataStore.js
@@ -5,8 +5,8 @@ export const store = createContext();
 function DataStore(props) {
     const [data, setData] = useState([]);
     const fetchdata = () => {
-        // return fetch("https://ecommerce-project-8m5d.onrender.com/api/findProduct")
-        return fetch("http://localhost:5008/api/findProduct")
+        return fetch("https://ecommerce-project-8m5d.onrender.com/api/findProduct")
+        // return fetch("http://localhost:5008/api/findProduct")
             .then((response) => response.json())
             .then((user) => setData(user))
     }
